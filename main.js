7267fb91ecbd8be3c8ddaf0cf263efc640b1499b
Denys Hutsal <denyshutsal@gmail.com>
2023-08-06 10:36:43 +0200

    rename functions and variables

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 "use script";
 
-// raname functions!----------------------------------
-
 const breakBtnMinus = document.querySelector(".break-btn-minus");
 const breakBtnPlus = document.querySelector(".break-btn-plus");
 const workBtnMinus = document.querySelector(".work-btn-minus");
@@ -10,39 +8,41 @@ const workTime = document.querySelector(".work-time");
 
 const resetTimerBtn = document.querySelector(".reset-timer-btn");
 const startTimerBtn = document.querySelector(".circle");
-const timer = document.querySelector(".timer");
+const timerText = document.querySelector(".timer");
 
 let start = 25;
 let workStartMin = 25;
 let workStartSec = 0;
 let endMin = 0;
 
-// variable to store our intervalID
-let nIntervId;
+// variable to store our interval
+let interval;
 
-startTimerBtn.addEventListener("click", changeColor);
+startTimerBtn.addEventListener("click", startTimer);
 resetTimerBtn.addEventListener("click", resetTimer);
+
 workBtnMinus.addEventListener("click", function () {
   start--;
   workTime.innerHTML = start;
   workStartMin = start;
 });
+
 workBtnPlus.addEventListener("click", function () {
   start++;
   workTime.innerHTML = start;
   workStartMin = start;
 });
 
-function changeColor() {
+function startTimer() {
   // check if an interval has already been set up
-  if (!nIntervId) {
-    nIntervId = setInterval(flashText, 1000);
+  if (!interval) {
+    interval = setInterval(timer, 1000);
   }
 }
 
-function flashText() {
+function timer() {
   if (workStartMin === endMin && workStartSec === 0) {
-    stopTextColor();
+    stopTimer();
     return;
   }
 
@@ -52,26 +52,26 @@ function flashText() {
   }
 
   startTimerBtn.classList.toggle("ripples");
-  timer.innerHTML = `${workStartMin}:${workStartSec}`;
+  timerText.innerHTML = `${workStartMin}:${workStartSec}`;
   workStartSec--;
 }
 
-function stopTextColor() {
-  clearInterval(nIntervId);
+function stopTimer() {
+  clearInterval(interval);
   // release our intervalID from the variable
-  nIntervId = null;
+  interval = null;
   startTimerBtn.classList.remove("ripples");
   workStartMin = start;
   workStartSec = 0;
-  timer.innerHTML = "00:00";
+  timerText.innerHTML = "00:00";
 }
 
 function resetTimer() {
-  clearInterval(nIntervId);
+  clearInterval(interval);
   // release our intervalID from the variable
-  nIntervId = null;
+  interval = null;
   startTimerBtn.classList.remove("ripples");
   workStartMin = start;
   workStartSec = 0;
-  timer.innerHTML = `${workStartMin}:${workStartSec}0`;
+  timerText.innerHTML = `${workStartMin}:${workStartSec}0`;
 }
